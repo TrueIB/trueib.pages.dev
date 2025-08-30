2876b223697912680bc22fee3062031c42b9d26d
TrueIB <18538252970@163.com>
2025-08-30 20:09:40 +0800

    Delete js directory

--- js/notesConfig.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("message", function (event) {
-  if (event.data && event.data.type === "THEME_CHANGE") {
-    this.document.documentElement.setAttribute("data-subject", event.data.subject);
-  }
-});
